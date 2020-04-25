d0f1c9df5bd22b7392d11d698a8b932cbdef8fd1
Aryan Agarwal <aryankush025@gmail.com>
2020-04-25 21:53:22 +0530

    header added as partials

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express configs
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views locations
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
   res.render('index', {
-    title: 'Index from hbs app',
+    title: 'Weather',
     name: 'Aryan Agarwal',
   })
 })
