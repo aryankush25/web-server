c0471df6a78ab9fed0c11201fa406ac7be88ec32
Aryan Agarwal <aryankush025@gmail.com>
2020-04-26 03:58:49 +0530

    weather forcast api fetched on FE with form

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
 console.log('Client side js file is loaded')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  message1.textContent = 'Loading...'
+  message2.textContent = ''
+
+  fetch(`http://localhost:3000/weather?address=${search.value}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          message1.textContent = data.error
+          console.log('Error', data.error)
+        } else {
+          message1.textContent = data.forecast
+          message2.textContent = data.location
+        }
+      })
+    }
+  )
+})
